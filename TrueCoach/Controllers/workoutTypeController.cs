7f57810e4e00d246af199f94c12e9fabe788adce
shalina2 <shalom.belaineh@gmail.com>
2019-02-04 13:09:59 -0800

    adding model for workouttype

--- TrueCoach/Controllers/workoutTypeController.cs
+++ TrueCoach/Controllers/workoutTypeController.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TrueCoach.Controllers
 {
-    public class workoutTypeController
+    public class workoutTypeController:Controller
     {
+     public workoutTypeController(workout)
     }
 }
