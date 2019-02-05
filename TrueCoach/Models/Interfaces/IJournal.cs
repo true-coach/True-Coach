a3ee9dc7eef78dd468fb7f30dd4ca0304b504a15
shalina2 <shalom.belaineh@gmail.com>
2019-02-05 08:36:18 -0800

    created interface

---
+++ TrueCoach/Models/Interfaces/IJournal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrueCoach.Models.Interfaces
+{
+    public interface IJournal
+    {
+        Task CreateJournal(journal Journal);
+
+        Task<journal> GetJournal(int id);
+        Task<IEnumerable<journal>> GetJournals();
+        Task DeleteJournal(int id);
+    }
+}
