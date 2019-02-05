b1d2ed5ed1bbe198e82b8172d4f4b49e394e4581
shalina2 <shalom.belaineh@gmail.com>
2019-02-04 16:16:11 -0800

    adding journal

---
+++ TrueCoach/Controllers/JournalController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrueCoach.Controllers
+{
+    public class JournalController
+    {
+        private readonly IJournal _context;
+
+            public JournalController(IJournal context)
+        {
+            _context = context;
+        }
+        public async Task<IAsyncResult> Index(string Entry)
+        {
+            var Entry = await _context.GetEntry();
+
+            if (!string.IsNullOrEmpty(Entry))
+            {
+                Entry=Entry.where()
+            }
+        }
+
+    }
+}
--- TrueCoach/Models/journal.cs
+++ TrueCoach/Models/journal.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TrueCoach.Controllers
+namespace TrueCoach.Models
 {
-    public class GoalController
+    public class journal
     {
     }
 }
