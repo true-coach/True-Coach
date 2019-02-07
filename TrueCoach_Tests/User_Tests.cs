04010c67ee382b010673dea4de172bfbd5ab92ae
Joseph <pumphrey.joseph@gmail.com>
2019-02-07 15:21:16 -0800

    User Model Tests are done

---
+++ TrueCoach_Tests/User_Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TrueCoach.Models;
+
+namespace TrueCoach_Tests
+{
+    public class User_Tests
+    {
+        [Fact]
+        public void CanGetUserID()
+        {
+            User user = new User();
+            user.ID = 1;
+            Assert.Equal(1, user.ID);   
+        }
+        [Fact]
+        public void CanSerUserID()
+        {
+            User user = new User();
+            user.ID = 1;
+            user.ID = 2;
+            Assert.Equal(2, user.ID);
+        }
+
+        [Fact]
+        public void CanGetUserUserName()
+        {
+            User user = new User();
+            user.UserName = "Name";
+            Assert.Equal("Name", user.UserName);
+        }
+        [Fact]
+        public void CanSerUserUserName()
+        {
+            User user = new User();
+            user.UserName = "First";
+            user.UserName = "Name";
+            Assert.Equal("Name", user.UserName);
+        }
+    }
+}
