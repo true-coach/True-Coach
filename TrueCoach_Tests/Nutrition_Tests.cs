0d7c9df6d330c91db7daeaa8256f9896f095b5a3
Joseph <pumphrey.joseph@gmail.com>
2019-02-07 15:17:43 -0800

    Regeneration Model Tests are done

---
+++ TrueCoach_Tests/Nutrition_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TrueCoach.Models;
+
+namespace TrueCoach_Tests
+{
+    public class Nutrition_Tests
+    {
+        [Fact]
+        public void CanGetNutritionID()
+        {
+        }
+    }
+}
