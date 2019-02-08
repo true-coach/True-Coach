110e9b387531080a2872d9fb7516fc43c0d14c04
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-02-07 19:32:38 -0800

    updated styling

--- TrueCoach/Startup.cs
+++ TrueCoach/Startup.cs
@@ -2,8 +2,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TrueCoach.Data;
+using TrueCoach.Models.Interfaces;
+using TrueCoach.Models.Services;
 
 namespace TrueCoach
 {
@@ -36,14 +40,14 @@ namespace TrueCoach
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-           // services.AddDbContext<TrueCoachJournalDbContext>(options =>
+            services.AddDbContext<TrueCoachJournalDbContext>(options =>
 
-           //options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+           options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
             services.AddDistributedMemoryCache();
             
             services.AddSession();
-           // services.AddTransient<IJournal, JournalServiceManagment>();
+           services.AddTransient<IJournal, JournalServiceManagment>();
             //services.AddTransient<IRegistration, RegistratoinServiceManagement>();
             services.AddTransient<IRegeneration, RegenerationManagementService>();
         }
